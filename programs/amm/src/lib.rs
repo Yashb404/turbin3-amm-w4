80b4dcaebedf20b9bfab4a03d29feb3c4884e60f
Yashb404 <yashbhardwaj7890@gmail.com>
2026-05-25 01:01:09 +0530

    init

---
+++ programs/amm/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("AmnNAqkC43JxdgbEYDaZsHJqr3vfywMupd7Y9e5C2tpJ");
+
+#[program]
+pub mod amm {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
